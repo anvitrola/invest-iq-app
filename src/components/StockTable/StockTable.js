31c0346abc5bc6a56e0485e6edd47d28d1c7809b
Ana Viana <aviana@safeops.io>
2023-10-10 22:12:44 -0300

    feat: add empty state handler to stock table on dashboard

--- src/components/StockTable/StockTable.js
+++ src/components/StockTable/StockTable.js
@@ -14,6 +14,7 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Loader from "../Loader/Loader";
+import FlexContainer from "../FlexContainer";
 
 const headers = [
   "Stock (Ticker)",
@@ -89,49 +90,83 @@ export default function StockTable({ refreshStockList, setRefreshStockList }) {
         </Typography>
       </Stack>
 
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead
-            sx={{ backgroundColor: "rgb(32 45 172 / 83%)", color: "white" }}
-          >
-            <TableRow>
-              {headers.map((header, idx) => (
-                <TableCell key={idx} align="left" sx={{ color: "whitesmoke" }}>
-                  <b>{header.toUpperCase()}</b>
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {monitoredStocksList.map((row) => (
-              <TableRow
-                key={row.ticker}
-                style={{ cursor: "pointer" }}
-                onClick={() => {
-                  navigate(`/history/${row.ticker}`);
-                }}
-              >
-                <TableCell
-                  component="th"
-                  scope="row"
-                  style={{ color: "var(--dark-blue)" }}
-                >
-                  <b>{row.ticker}</b>
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-
-                <TableCell align="left">{row.volume}</TableCell>
-                <TableCell align="left">{row.change}</TableCell>
-                <TableCell align="left">{row.lower_bound}</TableCell>
-                <TableCell align="left">{row.upper_bound}</TableCell>
-                <TableCell align="left">{row.close}</TableCell>
+      {monitoredStocksList.length ? (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead sx={{ backgroundColor: "rgb(32 45 172 / 83%)" }}>
+              <TableRow>
+                {headers.map((header, idx) => (
+                  <TableCell
+                    key={idx}
+                    align="left"
+                    sx={{ color: "whitesmoke" }}
+                  >
+                    <b>{header.toUpperCase()}</b>
+                  </TableCell>
+                ))}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {monitoredStocksList.map((row) => (
+                <TableRow
+                  key={row.ticker}
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    navigate(`/history/${row.ticker}`);
+                  }}
+                >
+                  <TableCell
+                    component="th"
+                    scope="row"
+                    style={{ color: "var(--dark-blue)" }}
+                  >
+                    <b>{row.ticker}</b>
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+
+                  <TableCell align="left">{row.volume}</TableCell>
+                  <TableCell align="left">{row.change}</TableCell>
+                  <TableCell align="left">{row.lower_bound}</TableCell>
+                  <TableCell align="left">{row.upper_bound}</TableCell>
+                  <TableCell align="left">{row.close}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <FlexContainer
+          flexDirection="column"
+          sx={{
+            padding: "4rem",
+            backgroundColor: "var(--white)",
+            height: "20rem",
+          }}
+        >
+          <Typography
+            variant="h5"
+            component="h5"
+            sx={{ color: "var(--dark-blue)", fontWeight: "600", mb: 3 }}
+          >
+            No stocks on your portfolio yet
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h6"
+            sx={{
+              color: "var(--dark-blue)",
+              width: "50%",
+              textAlign: "center",
+            }}
+          >
+            Don't worry, we're here to help! Make sure to add some stocks to
+            your portfolio and we'll keep you informed whenever the stock price
+            crosses your upper or lower limit.
+          </Typography>
+        </FlexContainer>
+      )}
     </Stack>
   );
 }
